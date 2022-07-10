9cdf2009379fbf4fcbc1c132c40e4cfd1f3ce2d5
Marvin Delroy Aroza <57531814+marvin-aroza@users.noreply.github.com>
2022-07-11 04:28:08 +0530

    added new modules

---
+++ src/app/_Modules/pages/influencer/influencer.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { InfluencerRoutingModule } from './influencer-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    InfluencerRoutingModule
+  ]
+})
+export class InfluencerModule { }
